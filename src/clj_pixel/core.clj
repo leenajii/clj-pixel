721cd8ef88d41b7292e601355bae3e6b96600a9c
Leena Wooller <leena.wooller@zervant.com>
2016-12-21 15:13:58 +0200

    WIP

--- src/clj_pixel/core.clj
+++ src/clj_pixel/core.clj
@@ -5,15 +5,28 @@
 (def width 180)
 
 (def file (slurp "resources/pixelarray"))
-(def arr (s/split file  #","))
+(def pixelvector (s/split file  #","))
 
 (defn getLocation [x y]
   (bit-shift-left (+ x (* width y)) 2))
 
+(defn check-color [data result x y]
+  (let [idx (getLocation x y)
+        rgb (nth data idx)
+        updated (assoc result idx rgb)]
+    (println rgb)
+    (println (filter some? updated))
+    updated))
+
+(defn check-color-conj [data result x y]
+  (let [concatenated (conj result (check-color data result x y))]
+    concatenated))
+
 (defn process [data]
   (dotimes [y height]
     (dotimes [x width]
-      (println (str "" (getLocation x  y))))))
+      (let [initial (vec (make-array Object (count data)))
+             result (check-color-conj data initial x y)]))))
 
 
 
