6fe62a74dcb748a9155c3cf93f75aa56e373b8c7
Leena Wooller <leena.wooller@zervant.com>
2016-12-22 13:48:00 +0200

    Stores new colors to result vector.

--- src/clj_pixel/traverse.clj
+++ src/clj_pixel/traverse.clj
@@ -2,11 +2,13 @@
   (:require [clojure.string :as s])
   (:require [clj-pixel.actions :as act]))
 
-(def height 60)
-(def width 180)
-
 (def file (slurp "resources/pixelarray"))
 (def pixelvector (s/split file  #","))
+
+(def result (atom pixelvector))
+
+(def height 60)
+(def width 180)
 
 (defn get-location [x y]
   (bit-shift-left (+ x (* width y)) 2))
@@ -14,16 +16,29 @@
 (defn get-rgb [data idx]
   (hash-map :r (nth data idx) :g (nth data (+ idx 1)) :b (nth data (+ idx 2))))
 
-(defn action? [rgb]
+(defn color [idx rgb]
+    (reset! result (assoc @result idx (get rgb :r)))
+    (reset! result (assoc @result (+ idx 1) (:g rgb)))
+    (reset! result (assoc @result (+ idx 2) (:b rgb)))
+    (reset! result (assoc @result (+ idx 3) 255)))
+
+(defn white [idx]
+  (color idx (hash-map :r 255 :g 255 :b 255)))
+
+(defn action? [rgb idx]
   (cond
-    (act/draw-up? rgb) (str "Draw up: " rgb)
-    (act/draw-left? rgb) (str "Draw left: " rgb)
-    (act/turn-left? rgb) (str "Turn left: " rgb)
-    (act/turn-right? rgb) (str "Turn right: " rgb)
-    (act/stop? rgb) (str "STOP: " rgb)
-    :else (str "No action: " rgb)))
+    (act/draw-up? rgb) (white idx)
+    (act/draw-left? rgb) (white idx)
+    (act/turn-left? rgb) (white idx)
+    (act/turn-right? rgb) (white idx)
+    (act/stop? rgb) (color idx (hash-map :r 55 :g 55 :b 55))
+    :else (color idx rgb)))
 
 (defn process []
   (dotimes [y height]
     (dotimes [x width]
-      (println (action? (get-rgb pixelvector (get-location x y)))))))
+      (let [idx (get-location x y)]
+        (action? (get-rgb pixelvector idx) idx))))
+  (println @result)
+  (println (count @result))
+  (spit "resources/result" (s/replace (s/replace @result #" " ",") #"\"" "")))
