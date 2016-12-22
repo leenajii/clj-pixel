0ca51e5b5aa60bbc21aa6a43a29e27255d27da33
Leena Wooller <leena.wooller@zervant.com>
2016-12-22 12:36:57 +0200

    Hash values to strings.

--- src/clj_pixel/actions.clj
+++ src/clj_pixel/actions.clj
@@ -1,12 +1,12 @@
 (ns clj-pixel.actions)
 
-(def left (hash-map :r 123, :g 131, :b 154))
-(def right (hash-map :r 182, :g 149, :b 72))
+(def left (hash-map :r "123" :g "131" :b "154"))
+(def right (hash-map :r "182" :g "149" :b "72"))
 
-(def draw-up (hash-map :r 7, :g 84, :b 19))
-(def draw-left (hash-map :r 139, :g 57, :b 137))
+(def draw-up (hash-map :r "7" :g "84" :b "19"))
+(def draw-left (hash-map :r "139" :g "57" :b "137"))
 
-(def stop (hash-map :r 51, :g 69, :b 169))
+(def stop (hash-map :r "51", :g "69", :b "169"))
 
 (defn turn-left? [rgb]
   (if (= rgb left)
@@ -28,7 +28,7 @@
     true
     false))
 
-(defn dstop? [rgb]
+(defn stop? [rgb]
   (if (= rgb stop)
     true
     false))
