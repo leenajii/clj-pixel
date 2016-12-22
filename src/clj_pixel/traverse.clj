e27e4285c923c60bd8be41ec72f061acb67baeb5
Leena Wooller <leena.wooller@zervant.com>
2016-12-22 21:41:47 +0200

    Traverse move.

--- src/clj_pixel/traverse.clj
+++ src/clj_pixel/traverse.clj
@@ -25,20 +25,35 @@
 (defn white [idx]
   (color idx (hash-map :r 255 :g 255 :b 255)))
 
-(defn action? [rgb idx]
+(defn draw-up [x y]
+  (dotimes [y height]
+    (let [idx (get-location x y)]
+      (if (not (act/stop? (get-rgb pixelvector idx)))
+        (white (get-location x y))))))
+
+(defn draw-left [x y]
+  (when (> x 0)
+    (let [idx (get-location x y)]
+    (if (act/stop? (get-rgb idx))
+      "I found it! Now I want to stop executing!"
+      (do
+        (white idx)
+        (recur (dec x)))))))
+
+(defn action? [rgb idx x y]
   (cond
-    (act/draw-up? rgb) (white idx)
-    (act/draw-left? rgb) (white idx)
+    (act/draw-up? rgb) (do (white idx)(draw-up x y))
+    (act/draw-left? rgb) (do (white idx)(draw-left x y))
     (act/turn-left? rgb) (white idx)
     (act/turn-right? rgb) (white idx)
     (act/stop? rgb) (color idx (hash-map :r 55 :g 55 :b 55))
-    :else (color idx rgb)))
+    :else (color idx {:r 0 :g 0 :b 0})))
 
 (defn process []
   (dotimes [y height]
     (dotimes [x width]
       (let [idx (get-location x y)]
-        (action? (get-rgb pixelvector idx) idx))))
+        (action? (get-rgb pixelvector idx) idx x y))))
   (println @result)
   (println (count @result))
   (spit "resources/result" (s/replace (s/replace @result #" " ",") #"\"" "")))
